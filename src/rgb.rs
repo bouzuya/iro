dbba87e7aab6c742e05fcd603677ea5afe2d2b5b
bouzuya <m@bouzuya.net>
2026-09-16 21:28:37 +0900

    Add some impls for Rgb

--- src/rgb.rs
+++ src/rgb.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct Rgb {
     r: u8,
     g: u8,
@@ -30,5 +31,35 @@ mod tests {
     fn test_rgb_to_hex() {
         let rgb = Rgb::new(255, 128, 0);
         assert_eq!(rgb.to_hex(), "#FF8000");
+    }
+
+    #[test]
+    fn test_impl_clone() {
+        fn assert_impl<T: Clone>() {}
+        assert_impl::<Rgb>();
+    }
+
+    #[test]
+    fn test_impl_copy() {
+        fn assert_impl<T: Copy>() {}
+        assert_impl::<Rgb>();
+    }
+
+    #[test]
+    fn test_impl_debug() {
+        fn assert_impl<T: std::fmt::Debug>() {}
+        assert_impl::<Rgb>();
+    }
+
+    #[test]
+    fn test_impl_eq() {
+        fn assert_impl<T: Eq>() {}
+        assert_impl::<Rgb>();
+    }
+
+    #[test]
+    fn test_impl_partial_eq() {
+        fn assert_impl<T: PartialEq>() {}
+        assert_impl::<Rgb>();
     }
 }
